f20c16b31e142484823d199b170ebc46bf444110
Joshua Boniface <joshua@boniface.me>
2019-04-25 23:24:00 -0400

    Add helpful text

--- build_plugin.py
+++ build_plugin.py
@@ -55,11 +55,11 @@ def build_plugin(project):
     os.chdir(project_dir)
 
     if build_cfg['build_type'] == 'dotnet':
-        stdout, stderr, retcode = build_command = "dotnet publish --configuration {} --framework {} --output ../bin/".format(
+        build_command = "dotnet publish --configuration {} --framework {} --output ../bin/".format(
             build_cfg['dotnet_configuration'],
             build_cfg['dotnet_framework']
         )
-        run_os_command(build_command)
+        stdout, stderr, retcode = run_os_command(build_command)
         print(stdout)
         print(stderr)
     elif build_cfg['build_type'] == 'build.py':
